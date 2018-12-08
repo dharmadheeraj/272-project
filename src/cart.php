3b9e706f63bf77489111460b097a4316bfe77d6e
Dharma Dheeraj Chintala <38597753+dharmadheeraj@users.noreply.github.com>
2018-12-07 21:13:26 -0800

    Adding PAypal and Cart Total

--- src/cart.php
+++ src/cart.php
@@ -43,33 +43,32 @@
 		<!-- Header desktop -->
 		<div class="container-menu-header">
 			<?php
-			//include 'nav.php';
+			include 'nav.php';
+			include_once('paypalConfig.php');
 			?>
 		</div>
 	</header>
 
 	<!-- Title Page -->
-	<section class="bg-title-page p-t-40 p-b-50 flex-col-c-m" style="background-image: url(images/heading-pages-01.jpg);">
+	<section class="bg-title-page p-t-40 p-b-50 flex-col-c-m" style="background-image: url(images/cart.jpg);">
 		<h2 class="l-text2 t-center">
-			Cart
+			Your Cart
 		</h2>
 	</section>
 
 	
 	<!-- Cart -->
 	<section class="cart bgwhite p-t-70 p-b-100">
-		<div class="container">
-
-
+		<div class="container" id="getcart">
+	
+			
 			<!-- Cart item -->
 			
-			<div class="container-table-cart pos-relative">
-				<div class="wrap-table-shopping-cart bgwhite">
-					<table class="table-shopping-cart" id="getcart">
+			
 						
 						
 
-						<tr class="table-row">
+						<!--<tr class="table-row">
 							<td class="column-1">
 								<div class="cart-img-product b-rad-4 o-f-hidden">
 									<img src="images/item-05.jpg" alt="IMG-PRODUCT">
@@ -91,108 +90,11 @@
 								</div>
 							</td>
 							<td class="column-5">$16.00</td>
-						</tr>
-					</table>
-				</div>
-			</div>
+						</tr>-->
+						
+				
 
-			<div class="flex-w flex-sb-m p-t-25 p-b-25 bo8 p-l-35 p-r-60 p-lr-15-sm">
-				<div class="flex-w flex-m w-full-sm">
-					<div class="size11 bo4 m-r-10">
-						<input class="sizefull s-text7 p-l-22 p-r-22" type="text" name="coupon-code" placeholder="Coupon Code">
-					</div>
-
-					<div class="size12 trans-0-4 m-t-10 m-b-10 m-r-10">
-						<!-- Button -->
-						<button class="flex-c-m sizefull bg1 bo-rad-23 hov1 s-text1 trans-0-4">
-							Apply coupon
-						</button>
-					</div>
-				</div>
-
-				<div class="size10 trans-0-4 m-t-10 m-b-10">
-					<!-- Button -->
-					<button class="flex-c-m sizefull bg1 bo-rad-23 hov1 s-text1 trans-0-4">
-						Update Cart
-					</button>
-				</div>
-			</div>
-
-			<!-- Total -->
-			<div class="bo9 w-size18 p-l-40 p-r-40 p-t-30 p-b-38 m-t-30 m-r-0 m-l-auto p-lr-15-sm">
-				<h5 class="m-text20 p-b-24">
-					Cart Totals
-				</h5>
-
-				<!--  -->
-				<div class="flex-w flex-sb-m p-b-12">
-					<span class="s-text18 w-size19 w-full-sm">
-						Subtotal:
-					</span>
-
-					<span class="m-text21 w-size20 w-full-sm">
-						$39.00
-					</span>
-				</div>
-
-				<!--  -->
-				<div class="flex-w flex-sb bo10 p-t-15 p-b-20">
-					<span class="s-text18 w-size19 w-full-sm">
-						Shipping:
-					</span>
-
-					<div class="w-size20 w-full-sm">
-						<p class="s-text8 p-b-23">
-							There are no shipping methods available. Please double check your address, or contact us if you need any help.
-						</p>
-
-						<span class="s-text19">
-							Calculate Shipping
-						</span>
-
-						<div class="rs2-select2 rs3-select2 rs4-select2 bo4 of-hidden w-size21 m-t-8 m-b-12">
-							<select class="selection-2" name="country">
-								<option>Select a country...</option>
-								<option>US</option>
-								<option>UK</option>
-								<option>Japan</option>
-							</select>
-						</div>
-
-						<div class="size13 bo4 m-b-12">
-						<input class="sizefull s-text7 p-l-15 p-r-15" type="text" name="state" placeholder="State /  country">
-						</div>
-
-						<div class="size13 bo4 m-b-22">
-							<input class="sizefull s-text7 p-l-15 p-r-15" type="text" name="postcode" placeholder="Postcode / Zip">
-						</div>
-
-						<div class="size14 trans-0-4 m-b-10">
-							<!-- Button -->
-							<button class="flex-c-m sizefull bg1 bo-rad-23 hov1 s-text1 trans-0-4">
-								Update Totals
-								</button>
-						</div>
-					</div>
-				</div>
-
-				<!--  -->
-				<div class="flex-w flex-sb-m p-t-26 p-b-30">
-					<span class="m-text22 w-size19 w-full-sm">
-						Total:
-					</span>
-
-					<span class="m-text21 w-size20 w-full-sm">
-						$39.00
-					</span>
-				</div>
-
-				<div class="size15 trans-0-4">
-					<!-- Button -->
-					<button class="flex-c-m sizefull bg1 bo-rad-23 hov1 s-text1 trans-0-4" href="confirmation.php">
-						Proceed to Checkout
-					</button>
-				</div>
+				
 			</div>
 		</div>
 	</section>
@@ -432,6 +334,154 @@
 			
 		}
 	</script>
+	
+	
+    <!-- PayPal In-Context Checkout script -->
+   <script src="//www.paypalobjects.com/api/checkout.js"></script>
+    <!-- PayPal In-Context Checkout script -->
+    <!--<script type="text/javascript">
+ 
+        var client = {
+            sandbox: '<?php echo(CLIENT_ID)?>'
+        };
+
+        var environment = 'sandbox';
+        var transaction = '';
+
+        function showDom(id) {
+            var arr;
+            if (!Array.isArray(id)) {
+                arr = [id];
+            } else {
+                arr = id;
+            }
+            arr.forEach(function (domid) {
+                document.getElementById(domid).style.display = 'initial';
+            });
+        }
+       function handleResponse(result) {
+            // document.getElementById('paypal-execute-details').textContent = JSON.stringify(result, null, 2);
+            // showDom('paypal-end');
+            var resultDOM = JSON.stringify(result, null, 2);
+            $json_response = result;
+            // console.log($json_response['id']);
+            var payID = $json_response['id'];
+
+            var paymentState = $json_response['state'];
+            var finalAmount = $json_response['transactions'][0]['amount']['total'];
+            var currency = $json_response['transactions'][0]['amount']['currency'];
+            var transactionID= $json_response['transactions'][0]['related_resources'][0]['sale']['id'];
+            var payerFirstName = $json_response['payer']['payer_info']['first_name'];
+            var payerLastName = $json_response['payer']['payer_info']['last_name'];
+            var recipientName= $json_response['payer']['payer_info']['shipping_address']['recipient_name'],FILTER_SANITIZE_SPECIAL_CHARS;
+            var addressLine1= $json_response['payer']['payer_info']['shipping_address']['line1'];
+            var addressLine2 = $json_response['payer']['payer_info']['shipping_address']['line2'];
+            var city= $json_response['payer']['payer_info']['shipping_address']['city'];
+            var state= $json_response['payer']['payer_info']['shipping_address']['state'];
+            var postalCode =$json_response['payer']['payer_info']['shipping_address']['postal_code'];
+            var transactionType = $json_response['intent'];
+            // var countryCode= filter_var($json_response['payer']['payer_info']['shipping_address']['country_code'],FILTER_SANITIZE_SPECIAL_CHARS);
+
+            //document.getElementById('paypal-execute-details-postal-code').textContent = postalCode; 
+            //document.getElementById('paypal-execute-details-state').textContent = state; 
+            //document.getElementById('paypal-execute-details-recipient-name').textContent = recipientName; 
+            //document.getElementById('paypal-execute-details-transaction-type').textContent = transactionType; 
+            //document.getElementById('paypal-execute-details-transaction-ID').textContent = transactionID; 
+            //document.getElementById('paypal-execute-details-first-name').textContent = payerFirstName; 
+            // document.getElementById('paypal-execute-details-last-name').textContent = payerLastName; 
+            //document.getElementById('paypal-execute-details-payment-state').textContent = paymentState;
+            //document.getElementById('paypal-execute-details-final-amount').textContent = finalAmount; 
+            //document.getElementById('paypal-execute-details-currency').textContent = currency; 
+            //document.getElementById('paypal-execute-details-addressLine1').textContent = addressLine1;
+            //document.getElementById('paypal-execute-details-addressLine2').textContent = addressLine2;
+            //document.getElementById('paypal-execute-details-city').textContent = city;
+
+
+            var paymentOptionTitle = document.getElementById('payment-selection-title');
+            var paymentOption = document.getElementById('payment-selection');
+            paymentOptionTitle.parentNode.style.display= 'none';
+            paymentOption.parentNode.style.dispaly='none';
+
+            showDom('paypal-end');
+
+        }
+
+
+ paypal.Button.render({
+
+        // Set your environment
+
+        env: 'sandbox', // sandbox | production
+
+        client: {
+            sandbox: '<?php echo(CLIENT_ID)?>'
+        },
+
+        // Set to 'Pay Now'
+
+        commit: true,
+
+        // Wait for the PayPal button to be clicked
+
+        payment: function(actions) {
+
+            var fullname = "Dharma";
+            var line1 = "test paypal Integration";
+            var line2 = "Test paypal Integration";
+            var shipToCity = "San JOse";
+            var shipToState = "CA";
+            var shipToZip="95126";
+            var shipToCountry="US";
+            var shippingSel = "";
+            var shipping_amt = 1;
+            var total_amt = 20;
+
+			return actions.payment.create({
+				meta: {
+					partner_attribution_id: '<?php echo(SBN_CODE)?>'
+				},
+				payment: {
+					transactions: [
+						{
+							amount: {
+								total: total_amt ,
+								currency: 'USD',
+								details:
+								{
+									subtotal: total_amt,
+									shipping: shipping_amt,
+								}
+							},
+							item_list:
+							{
+								shipping_address:
+								{
+									recipient_name: fullname,
+									line1: line1,
+									line2: line2,
+									city: shipToCity,
+									country_code: shipToCountry,
+									postal_code: shipToZip,
+									phone: "011862212345678",
+									state: shipToState,
+								}
+							}
+						}
+					]
+				}
+			});
+
+        },
+
+        // Wait for the payment to be authorized by the customer
+
+        onAuthorize: function(data, actions) {
+
+            return actions.payment.execute().then(handleResponse);
+        }
+
+    }, '#myContainer');
+     </script>-->
 
 </body>
 </html>
